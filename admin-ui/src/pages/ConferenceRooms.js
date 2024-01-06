9d0cc4a75539caed33392f2c4a88e36c10c933e8
RavinduBA <e19004@eng.pdn.ac.lk>
2024-01-07 02:48:47 +0530

    Conference Room halfly done with CardSlider

--- admin-ui/src/pages/ConferenceRooms.js
+++ admin-ui/src/pages/ConferenceRooms.js
@@ -1,11 +1,30 @@
-import React from "react";
+import "./App.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import SessionCards from "../components/SessionCards";
+import { productData, responsive } from "./data";
 
-const ConferenceRooms = () => {
+
+export default function ConferenceRooms() {
+
+    const product = productData.map((item) => (
+        <SessionCards
+            name={item.name}
+            url={item.imageurl}
+            price={item.price}
+            description={item.description}
+        />
+    ));
+
     return (
-        <div>
-            <h1>Conference Rooms.</h1>
+
+        <div className="Appss">
+
+            <Carousel showDots={true} responsive={responsive}>
+                {product}
+            </Carousel>
         </div>
     );
 };
 
-export default ConferenceRooms;
+
